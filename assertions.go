c9c60f853c71c58c935c3eddd5f7b465e4498b77
treyvon <Eugenewhitaker2022@gmail.com>
2024-02-11 21:16:59 -0500

    feat(assertions): impl more assert equals

--- assertions.go
+++ assertions.go
@@ -1,9 +1,46 @@
 package assertions
 
-import "testing"
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
 
-func AssertEquals(tb testing.TB, expected, actual string) {
-	if expected != actual {
-		tb.Fatalf("expected=%s, actual=%s", expected, actual)
+type Executable func() (string, bool)
+
+/*****************************************************************************
+ *                              PUBLIC FUNCTIONS                             *
+ *****************************************************************************/
+
+func AssertBoolEquals(tb testing.TB, expected, actual bool, msg string) {
+	if !assertEquals(expected, actual) {
+		tb.Fatalf(msg+" - expected=%t, actual=%t", expected, actual)
 	}
+}
+
+func AssertStringEquals(tb testing.TB, expected, actual, msg string) {
+	if !assertEquals(expected, actual) {
+		tb.Fatalf(msg+" - expected=%s, actual=%s", expected, actual)
+	}
+}
+
+func AssertAll(tb testing.TB, msg string, executables ...Executable) {
+	var failures []string
+	for i, executable := range executables {
+		if err, ok := executable(); ok {
+			failures = append(failures, fmt.Sprintf("test[%d] - %s", i, err))
+		}
+	}
+
+	if len(failures) != 0 {
+		tb.Fatalf(fmt.Sprintf("%s (%d failures)\n", msg, len(failures)) + strings.Join(failures, "\n"))
+	}
+}
+
+/*****************************************************************************
+ *                             PRIVATE FUNCTIONS                             *
+ *****************************************************************************/
+
+func assertEquals(expected, actual interface{}) bool {
+	return expected == actual
 }
