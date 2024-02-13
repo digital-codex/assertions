82125d2c0364c35149df98457864992e38af3bf4
treyvon <Eugenewhitaker2022@gmail.com>
2024-02-12 21:40:19 -0500

    feat(assertions): impl more assert equals

--- assertions.go
+++ assertions.go
@@ -3,7 +3,6 @@ package assertions
 import (
 	"fmt"
 	"reflect"
-	"strings"
 	"testing"
 )
 
@@ -15,32 +14,37 @@ type Executable func() (string, bool)
 
 func AssertBoolEquals(tb testing.TB, expected, actual bool, msg string) {
 	if expected != actual {
-		tb.Fatalf(msg+" - expected=%t, actual=%t", expected, actual)
+		tb.Fatal(msg + fmt.Sprintf(" - expected=%t, actual=%t", expected, actual))
+	}
+}
+
+func AssertIntEquals(tb testing.TB, expected, actual int, msg string) {
+	if expected != actual {
+		tb.Fatal(msg + fmt.Sprintf(" - expected=%d, actual=%d", expected, actual))
+	}
+}
+
+func AssertInt64Equals(tb testing.TB, expected, actual int64, msg string) {
+	if expected != actual {
+		tb.Fatal(msg + fmt.Sprintf(" - expected=%d, actual=%d", expected, actual))
 	}
 }
 
 func AssertStringEquals(tb testing.TB, expected, actual, msg string) {
 	if expected != actual {
-		tb.Fatalf(msg+" - expected=%s, actual=%s", expected, actual)
+		tb.Fatal(msg + fmt.Sprintf(" - expected=%s, actual=%s", expected, actual))
 	}
 }
 
-func AssertStructEquals(tb testing.TB, expected, actual interface{}, msg string) {
+func AssertStructEquals(tb testing.TB, expected, actual *interface{}, msg string) {
+	if expected != actual {
+		tb.Fatal(msg + fmt.Sprintf(" - expected=%+v, actual=%+v", expected, actual))
+	}
+}
+
+func AssertDeepEquals(tb testing.TB, expected, actual interface{}, msg string) {
 	if !reflect.DeepEqual(expected, actual) {
-		tb.Fatalf(msg+" - expected=%+v, actual=%+v", expected, actual)
-	}
-}
-
-func AssertAll(tb testing.TB, msg string, executables ...Executable) {
-	var failures []string
-	for i, executable := range executables {
-		if err, ok := executable(); ok {
-			failures = append(failures, fmt.Sprintf("test[%d] - %s", i, err))
-		}
-	}
-
-	if len(failures) != 0 {
-		tb.Fatalf(fmt.Sprintf("%s (%d failures)\n", msg, len(failures)) + strings.Join(failures, "\n"))
+		tb.Fatal(msg + fmt.Sprintf(" - expected=%+v, actual=%+v", expected, actual))
 	}
 }
 
