e56e01a83055c4b085a3d1b34ce2746f650e532d
treyvon <Eugenewhitaker2022@gmail.com>
2024-02-13 23:51:45 -0500

    feat(assertions): impl nil check

--- assertions.go
+++ assertions.go
@@ -6,11 +6,15 @@ import (
 	"testing"
 )
 
-type Executable func() (string, bool)
-
 /*****************************************************************************
  *                              PUBLIC FUNCTIONS                             *
  *****************************************************************************/
+
+func AssertNotNull(tb testing.TB, val any, msg string) {
+	if isNil(val) {
+		tb.Fatal(msg + fmt.Sprintf(" - expected not %+v", val))
+	}
+}
 
 func AssertBoolEquals(tb testing.TB, expected, actual bool, msg string) {
 	if expected != actual {
@@ -36,13 +40,13 @@ func AssertStringEquals(tb testing.TB, expected, actual, msg string) {
 	}
 }
 
-func AssertStructEquals(tb testing.TB, expected, actual *interface{}, msg string) {
+func AssertEquals(tb testing.TB, expected, actual any, msg string) {
 	if expected != actual {
 		tb.Fatal(msg + fmt.Sprintf(" - expected=%+v, actual=%+v", expected, actual))
 	}
 }
 
-func AssertDeepEquals(tb testing.TB, expected, actual interface{}, msg string) {
+func AssertDeepEquals(tb testing.TB, expected, actual any, msg string) {
 	if !reflect.DeepEqual(expected, actual) {
 		tb.Fatal(msg + fmt.Sprintf(" - expected=%+v, actual=%+v", expected, actual))
 	}
@@ -51,3 +55,17 @@ func AssertDeepEquals(tb testing.TB, expected, actual interface{}, msg string) {
 /*****************************************************************************
  *                             PRIVATE FUNCTIONS                             *
  *****************************************************************************/
+
+func isNil(val any) bool {
+	ret := false
+
+	if val == nil {
+		ret = true
+	}
+
+	switch reflect.TypeOf(val).Kind() {
+	case reflect.Array, reflect.Chan, reflect.Map, reflect.Ptr, reflect.Slice:
+		ret = reflect.ValueOf(val).IsNil()
+	}
+	return ret
+}
